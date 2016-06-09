c6e316c108f48a0c882e9b775b06ac317342709c
bim2 <sisbas@yahoo.com>
2016-06-09 19:17:55 +0300

    Index page done

---
+++ Aydinlik_Bilgi_Islem/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using Aydinlik_Bilgi_Islem.Models.db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Aydinlik_Bilgi_Islem.Controllers
+{
+    public class HomeController : Controller
+    {
+        genelEntities ent = new genelEntities();
+        // GET: Home
+        public ActionResult Index()
+        {                       
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(GenelBilgi usr)
+        {            
+            ent.GenelBilgi.Add(usr);
+            ent.SaveChanges();
+            Response.Write("<script>alert('İşleminiz kısa süre içersinde gerçekleştirilecektir</script>'");
+            return RedirectToAction("Index", "Home");
+        }
+        public ActionResult Administrator()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Administrator(GenelBilgi usr)
+        {
+            var bilgiler = (from i in ent.GenelBilgi.Select(i => i.ad)
+                            select i).ToList();
+            return View(bilgiler);
+        }
+
+    }
+}
